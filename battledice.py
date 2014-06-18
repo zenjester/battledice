a5904d444f208537cb4c73c12b731c14a46c0cc5
andrew <andrew@thinkbang.(none)>
2014-06-18 21:48:18 +0100

    army creation functions added

--- battledice.py
+++ battledice.py
@@ -1,4 +1,11 @@
 # battledice.py a simple test python game
 # andyp 11.04.14
+def compCreate():
+    return('creating computer team')
+
+def playerCreate():
+    return('create player team')
 
 print('welcome to battledice')
+print(compCreate())
+print(playerCreate())
