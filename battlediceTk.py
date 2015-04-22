c288a969e2d1a084c12879b38f1b9a745291478b
zenjester <zenjester@gmail.com>
2015-04-22 20:08:10 +0100

    change to using function to write lines

--- battlediceTk.py
+++ battlediceTk.py
@@ -11,20 +11,22 @@ def compCreate():
 def playerCreate():
     return 'create player team'
 
-def writeToWindow(msg):
-	numlines = msgArea.index('end -1 line').split('.')[0]
-	msgArea['state'] = 'normal'
+
+def writeToWindow(msg, quad):
+    numlines = quad.index('end -1 line').split('.')[0]
+    quad['state'] = 'normal'
 	if numlines==24: #TODO fix this for my window lines maybe a passed variable
-		msgArea.delete(1.0,2.0)
-	if msgArea.index('end-1c')!='1.0':
-		msgArea.insert('end','\n')
-	msgArea.insert('end',msg)
-	msgArea['state'] = 'disabled'
+        quad.delete(1.0, 2.0)
+    if quad.index('end-1c') != '1.0':
+        quad.insert('end', '\n')
+    quad.insert('end', msg)
+    quad['state'] = 'disabled'
 	
 """main function """
 turn = 0  # turn variable
 playerTxt = playerCreate()
 aiTxt = compCreate()
+aiTxt2 = 'sTest Piece of Text'
 #msgTxt = 'Welcome to the game \n'
 rollTxt = 'the roll is ' + dieRoller.dirRoll(6)
 app = tkinter.Tk()
@@ -35,11 +37,13 @@ msgArea = tkinter.Text(app, height=20, width=50, bg='cyan')
 rollArea = tkinter.Text(app, height=10, width=50, bg='yellow')
 playerArea.insert(1.0, 'Player Team\n')
 playerArea.insert(2.0, playerTxt)
-writeToWindow('living in a cloud')
-writeToWindow('Monster munch time boys')
-aiArea.insert(2.9, aiTxt)
+writeToWindow('living in a cloud', msgArea)
+writeToWindow('Monster munch time boys', msgArea)
+writeToWindow(aiTxt, aiArea)
+writeToWindow(aiTxt2, aiArea)
+
 #msgArea.insert('1.0', msgTxt)
-#msgArea.insert('2.0', msgTxt)
+# msgArea.insert('2o.0', msgTxt)
 rollArea.insert(0.0, rollTxt)
 turnBtn = tkinter.Button(app, text='Next Turn', width=10)
 saveBtn = tkinter.Button(app, text='save', width=10)
